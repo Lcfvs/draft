5d02c75af556478618ee96dd5903071be40e9898
Lcf.vs <michael.rouges@gmail.com>
2022-04-19 21:50:38 +0200

    Update mutations.js

--- mutations.js
+++ mutations.js
@@ -1,119 +1,61 @@
-const contexts = new WeakMap()
-const generators = new WeakMap()
 const mappers = new WeakMap()
 const matchers = new WeakMap()
 const observers = new WeakMap()
 const resolvers = new WeakMap()
-const targets = new WeakMap()
 let picked
 
-export const mutations = (
+export const mutations = async function* (
   node,
   target,
   mapper,
   options
-) => {
-  const context = { mapper, node, options, target }
-  const generator = generate(context)
-
-  contexts.set(context, generator)
-
-  return generator
-}
-
-export const log = () => {
-  console.log({
-    generators,
-    mappers,
-    matchers,
-    observers,
-    resolvers,
-    targets
-  })
-}
-
-const generate = async function* (
-  context
 ) {
-  const generator = contexts.get(context)
-  const { mapper, node, options, target } = context
   const { ownerDocument } = target
   const document = ownerDocument ?? target
+  const { defaultView } = document
+  const { MutationObserver } = defaultView
   const matches = current => current === node
-  const observer = observe(document, target, options)
-  const observed = generators.get(observer)
+  const observer = new MutationObserver(callback)
   const promises = []
   const resolver = [promises]
 
-  contexts.delete(context)
-  mappers.set(generator, mapper)
-  matchers.set(generator, matches)
-  resolvers.set(generator, resolver)
-  targets.set(generator, target)
-  observed.add(generator)
-  observer.observe(target, options)
+  mappers.set(observer, mapper)
+  matchers.set(observer, matches)
+  resolvers.set(observer, resolver)
   resolve(resolver)
+  observer.observe(target, options)
 
   try {
     while (true) {
       yield promises.shift()
     }
   } finally {
-    mappers.delete(generator)
-    matchers.delete(generator)
-    resolvers.delete(generator)
-    targets.delete(generator)
-    observed.delete(generator)
-
-    if (!observed.size) {
-      observer.disconnect()
-      generators.delete(observer)
-      observers.delete(document)
-    }
+    observer.disconnect()
+    mappers.delete(observer)
+    matchers.delete(observer)
+    resolvers.delete(observer)
   }
 }
 
-const observe = (
-  document,
-  target,
-  options
-) => {
-  if (!observers.has(document)) {
-    const { defaultView } = document
-    const { MutationObserver } = defaultView
-    const observer = new MutationObserver(callback)
-
-    generators.set(observer, new Set())
-    observers.set(document, observer)
-  }
-
-  const observer = observers.get(document)
-
-  observer.observe(target, options)
-
-  return observer
+export const log = () => {
+  console.log({
+    mappers,
+    matchers,
+    observers,
+    resolvers
+  })
 }
 
 const callback = (
   records,
   observer
 ) => {
-  for (const generator of generators.get(observer)) {
-    const mapper = mappers.get(generator)
-    const matches = matchers.get(generator)
-    const resolver = resolvers.get(generator)
-    const target = targets.get(generator)
-    const filtered = []
+  const mapper = mappers.get(observer)
+  const matches = matchers.get(observer)
+  const resolver = resolvers.get(observer)
 
-    for (const record of records) {
-      if (target.contains(record.target)) {
-        filtered.push(record)
-      }
-    }
-
-    for (const state of mapper(filtered, matches)) {
-      resolve(resolver, state)
-    }
+  for (const state of mapper([...records], matches)) {
+    resolve(resolver, state)
   }
 }
 
